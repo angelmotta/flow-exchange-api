779171ff216cd33c6e731b32e344f966e33d0096
Angel Motta <angel.motta@utec.edu.pe>
2023-12-18 21:39:04 -0500

    feat: spring-boot project

---
+++ src/main/java/com/example/flowapi/FlowApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flowapi/FlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
